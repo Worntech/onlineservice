f60c821fef62540f51cef892f34e28f6b36225e0
Worntech <pngiliule02@gmail.com>
2024-11-11 22:44:45 +0300

    fourth commit

--- web/models.py
+++ web/models.py
@@ -64,10 +64,10 @@ class MyUser(AbstractBaseUser):
     date_joined=models.DateTimeField(verbose_name="date joined", auto_now_add=True)
     
     last_login=models.DateTimeField(verbose_name="last login", auto_now=True)
-    is_admin=models.BooleanField(default=True)
+    is_admin=models.BooleanField(default=False)
     is_active=models.BooleanField(default=True)
-    is_staff=models.BooleanField(default=True)
-    is_superuser=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+    is_superuser=models.BooleanField(default=False)
     hide_email = models.BooleanField(default=True)
     
 
