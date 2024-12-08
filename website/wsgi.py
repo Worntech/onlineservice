281643280728527513a9cd0842eb8af2335d2b87
Worntech <pngiliule02@gmail.com>
2024-12-08 10:17:04 +0000

    Ignore .pyc files

--- website/wsgi.py
+++ website/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'website' project.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
 
+# Get the WSGI application for the project.
 application = get_wsgi_application()
